7ae0c43c2392f930793af9d8397fd8b4a88c20f4
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-12-07 20:36:14 +0200

    task-1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" This will show some basic annotations concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This will returns sum of two str """
+    return str1 + str2
