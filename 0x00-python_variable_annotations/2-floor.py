b76a7be95ca319ce29b17ac8c9a4c422a0087818
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-12-07 20:40:20 +0200

    task-2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This will show some basic annotations concat """
+
+import math
+
+
+def floor(n: float) -> int:
+    """ This will return the floor of float """
+    return math.floor(n)
