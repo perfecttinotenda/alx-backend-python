1d0af556bb3cfd87faef6cfcbd2d86a4b331297d
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-12-07 20:42:37 +0200

    Task-3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This will show some basic annotations - to string  """
+
+
+def to_str(n: float) -> str:
+    """ This returns the str representation of a float. """
+    return str(n)
