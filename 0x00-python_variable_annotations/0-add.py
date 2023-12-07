be4db6ac539bab4e92ea36d12bcf38c297e32175
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-12-07 20:26:58 +0200

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+    This will execute Basic annotations for vars.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        It will return sum of two floats.
+    '''
+    return a + b
