ed287e44bfd11f81fc8c38f5826580b7edd522a6
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-12-11 23:10:28 +0200

    fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-''' Description: Introduction to asynchronous programming in Python.
-    
+''' Description: Introduction to asynchronous programming in Python.   
     Author: Tinotenda
+                asynchronous coroutine that takes in an integer arg
+                 (max_delay, with a default value of 10) named wait_random
+                 that waits for a random delay between 0 and max_delay
+                 (included and float value) seconds and eventually returns it
+    Args: max_delay: int = 10
 '''
 
 import asyncio
