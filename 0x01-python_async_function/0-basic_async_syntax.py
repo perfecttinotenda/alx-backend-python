f016b8ba8e4af83d1fd5bd88c3db1925ee89a255
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-12-11 22:06:03 +0200

    task0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+    Introduction to asynchronous programming in Python.
+    Author: Tinotenda
+'''
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Delays execution for a random duration between 0 and max_delay (inclusive, float value),
+    then returns the delay duration.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
