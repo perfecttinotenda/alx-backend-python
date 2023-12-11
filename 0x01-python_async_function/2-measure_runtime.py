f5dc05de4d8c8b0e54a4fa76e740b4cc533d4c80
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-12-11 23:17:32 +0200

    task 2 fix

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
-""" Introduction to asynchronous programming 
+'''
+Description: Import wait_random from the previous python file and define an
+             async routine called wait_n. This routine takes two int arguments:
+             max_delay and n. It spawns wait_random n times with the specified
+             max_delay.
+
+             wait_n returns a list of delays (float values) in ascending order,
+             without using sort() due to concurrency.
     Author: Tinotenda
-"""
+    Arguments: n: int, max_delay: int = 10
+'''
+
+import asyncio
+from typing import List
+from asyncio import create_task
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-from asyncio import run
-from time import time
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """Waits for random delays up to max_delay, returns a list of delays."""
+    spawn_list = []
+    delay_list = []
 
-wait_n = __import__('./1-concurrent_coroutines.py').wait_n
+    for _ in range(n):
+        delayed_task = create_task(wait_random(max_delay))
+        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
+        spawn_list.append(delayed_task)
 
+    for spawn in spawn_list:
+        await spawn
 
-def measure_time(n: int, max_delay: int) -> float:
-    """ Measure the runtime for concurrent coroutines """
-    start_time = time()
-
-    run(wait_n(n, max_delay))
-
-    end_time = time()
-
-    return (end_time - start_time) / n
+    return delay_list
